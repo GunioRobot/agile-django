6152a3284f985583514dab30264a6a00bb112745
Andrés Torres <andres.torres.marroquin@gmail.com>
2011-03-02 22:47:02 -0600

    The project now is ready for use Django internationalization. Added 'es' to locale languages.

--- agile/views.py
+++ agile/views.py
@@ -1,6 +1,8 @@
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.utils.translation import ugettext as _
+
+# Create your views here.
 
 def index(request):
     return render_to_response('index.html')
