dfbf8ef7e1ea2da781421dbb3f44742d948f7990
Gerardo Orozco <gerardo.orozco.mosqueda@gmail.com>
2011-08-01 13:30:41 -0500

    S#16 Improve forms style to be cleaner.

--- agile/forms.py
+++ agile/forms.py
@@ -2,23 +2,23 @@ from agile.models import *
 from django import forms
 
 class ProjectForm(forms.ModelForm):
-    
+
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
         label_from_instance = lambda obj: obj.agile_get_name()
         self.fields['members'].label_from_instance = label_from_instance
-    
+
     class Meta:
         model = Project
         exclude = ('owner', 'is_active',)
-        
+
 class PhaseForm(forms.ModelForm):
     class Meta:
         model = Phase
         exclude = ('project', 'is_backlog', 'is_archive', 'index')
-        
+
 class StoryForm(forms.ModelForm):
-    
+
     def __init__(self, project=None, *args, **kwargs):
         super(StoryForm, self).__init__(*args, **kwargs)
         if project:
@@ -30,31 +30,31 @@ class StoryForm(forms.ModelForm):
         label_from_instance = lambda obj: obj.agile_get_name()
         self.fields['creator'].label_from_instance = label_from_instance
         self.fields['owner'].label_from_instance = label_from_instance
-        
+
     class Meta:
         model = Story
         exclude = ('index', 'number', 'blocked', 'color', 'ready')
-        
+
 class TaskForm(forms.ModelForm):
-    
+
     def __init__(self, project=None, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
         if project:
             self.fields['finished_by'].queryset = project.users
             self.fields['story'].queryset = project.stories
-    
+
     class Meta:
         model = Task
-        
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ('user', 'story',)
-        
+
 class FilterForm(forms.ModelForm):
     class Meta:
         model = Filter
-        
+
 class UserDataForm(forms.ModelForm):
     class Meta:
         model = User
